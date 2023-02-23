e455c1cf117aa5976b4c4a409758ee851d929117
Sunil Band <sunilband0007@gmail.com>
2023-02-23 09:46:01 +0530

    about and bgcircle

--- components/About.tsx
+++ components/About.tsx
@@ -26,16 +26,16 @@ const About = (props: Props) => {
   return (
     <div className="w-screen h-screen">
     <motion.div
-      initial={{
+       initial={{
         opacity: 0,
       }}
       whileInView={{
         opacity: 1,
         scale: [0.5, 1.1, 1],
       }}
-      transition={{ 
+      transition={{
         type: "spring",
-        duration: 1 
+        duration: 1.5,
       }}
       viewport={{ once: true }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row  px-10 justify-evenly mx-auto items-center z-20"
@@ -44,52 +44,75 @@ const About = (props: Props) => {
         About
       </h3>
 
-      <motion.div></motion.div>
+     
       <motion.div
-        initial={{
-          y: -200,
-          opacity: 0,
-        }}
-        whileInView={{
-          y: 0,
-          opacity: 1,
-        }}
-        transition={{
-          duration: 1,
-        }}
-        viewport={{ once: true }}
-      >
-        <motion.div
-          style={{
-            display: "flex",
-            placeItems: "center",
-            placeContent: "center",
-            perspective: 1000,
+          initial={{
+            y: -200,
+            opacity: 0,
           }}
-          onMouseMove={handleMouse}
-          onMouseLeave={mouseLeave}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{
+            duration: 1,
+          }}
+          viewport={{ once: true }}
         >
           <motion.div
             style={{
-              rotateX: rotateX,
-              rotateY: rotateY,
+              display: "flex",
+              placeItems: "center",
+              placeContent: "center",
+              perspective: 1000,
             }}
-            className="mt-28 flex items-start justify-center h-60 w-64 md:w-96 md:h-96"
+            onMouseMove={handleMouse}
+            onMouseLeave={mouseLeave}
           >
-            <Image
-              src={aboutImage}
-              priority={true}
-              alt="Sunil's about image"
-              className="-mb-10 md:mb-0 flex-shrink-0 w-64 h-64 rounded-full md:w-96 md:h-96 xl:scale-110  md:rounded-lg object-cover hover:scale-125 hover:drop-shadow-[0_0px_35px_#ffffff2f] text-[#ffffff5d] transition ease-in-out duration-700"
-            />
+            <motion.div
+              style={{
+                rotateX: rotateX,
+                rotateY: rotateY,
+              }}
+              className="mt-28 flex h-60 w-64 items-start justify-center md:h-96 md:w-96"
+            >
+              <Image
+                src={aboutImage}
+                priority={true}
+                alt="Sunil's about image"
+                className="-mb-10 h-64 w-64 flex-shrink-0 rounded-full object-cover text-[#ffffff5d] transition duration-700  ease-in-out hover:scale-125 hover:drop-shadow-[0_0px_35px_#ffffff2f] md:mb-0 md:h-96 md:w-96 md:rounded-lg xl:scale-110"
+              />
+            </motion.div>
           </motion.div>
         </motion.div>
-      </motion.div>
 
-      <div className="space-y-6 px-0 md:px-5">
-        <h4 className="text-xl sm:text-4xl font-semibold text-[#F7AB0A] xl:mt-20">{`Here's Something `}</h4>
-        <p className="sm:text-base md:max-w-xl text-sm md:text-base xl:text-xl leading-6  md:leading-7 xl:leading-8  text-justify">Hey there, this is Sunil. I am a B.Tech(hons) student and a Front End Developer with a strong background in HTML, CSS, JavaScript, and modern web technologies like React.js, Next.js, Bootstrap and Tailwind CSS. I have a keen eye for detail and a focus on delivering exceptional user experiences. {`I'm`} passionate about creating visually stunning and intuitive web experiences. I am a lifelong learner, constantly seeking new challenges and opportunities to grow as a developer.</p>
-      </div>
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+            scale: [0.5, 1.1, 1],
+          }}
+          transition={{
+            type: "spring",
+            duration: 2,
+          }}
+          viewport={{ once: true }}
+          className="space-y-6 px-0 md:px-5"
+        >
+          <h4 className="text-xl font-semibold text-[#F7AB0A] sm:text-4xl xl:mt-20">{`Here's Something `}</h4>
+          <p className="text-justify text-sm font-bold leading-6 antialiased sm:text-base sm:font-normal md:max-w-xl md:text-base  md:leading-7 xl:text-xl  xl:leading-8">
+            Hey there, this is Sunil. I am a B.Tech(hons) student and a Front
+            End Developer with a strong background in HTML, CSS, JavaScript, and
+            modern web technologies like React.js, Next.js, Bootstrap and
+            Tailwind CSS. I have a keen eye for detail and a focus on delivering
+            exceptional user experiences. {`I'm`} passionate about creating
+            visually stunning and intuitive web experiences. I am a lifelong
+            learner, constantly seeking new challenges and opportunities to grow
+            as a developer.
+          </p>
+        </motion.div>
     </motion.div>
     </div>
   );
